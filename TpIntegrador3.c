a8a77ebf65ab49d1f840ac9a1a624ceacd777c30
cinti.av <cin899a@gmail.com>
2022-10-29 20:40:33 -0300

    arreglos de subprocesos

--- TpIntegrador3.c
+++ TpIntegrador3.c
@@ -9,13 +9,12 @@ struct alumno
 
 struct alumno lista_alumnos[10];
 
+int dni_ingresado;
+int nota_ingresada;
 int cant_alumnos = 2;
-float dni_ingresado;
-float nota_ingresada;
 int bandera = 0;
 int validacion;
-float nueva_nota;
-int numero_alumno;
+int nueva_nota;
 
 void cargarDatos();
 void buscarAlumno();
@@ -27,6 +26,7 @@ void main()
     int opcion;
     do
     {
+        printf("\n");
         printf("Ingrese una opci%cn \n", 162);
         printf("1-Cargar alumnos \n");
         printf("2-Buscar un alumno \n");
@@ -44,13 +44,10 @@ void main()
                 break;
 
             case 2: // búsqueda
-
                 buscarAlumno();
-
                 break;
 
             case 3: // modificación de notas de alumnos
-
                 modificarAlumnos();
                 break;
 
@@ -76,7 +73,7 @@ void cargarDatos()
             scanf("%i", &lista_alumnos[i].dni);
             if (lista_alumnos[i].dni < 1000000 || lista_alumnos[i].dni > 99999999)
             {
-                printf("ERROR: no ingreso un dni valido");
+                printf("ERROR: no ingres%c un dni valido\n", 162);
             }
 
         } while (lista_alumnos[i].dni < 1000000 || lista_alumnos[i].dni > 99999999);
@@ -87,9 +84,8 @@ void cargarDatos()
             scanf("%i", &lista_alumnos[i].nota);
             if (lista_alumnos[i].nota < 1 || lista_alumnos[i].nota > 10)
             {
-                printf("ERROR: no ingreso una nota valida");
+                printf("ERROR: no ingres%c una nota valida\n", 162);
             }
-
         } while (lista_alumnos[i].nota < 1 || lista_alumnos[i].nota > 10);
     }
 }
@@ -97,58 +93,64 @@ void cargarDatos()
 void buscarAlumno()
 {
     printf("ingrese DNI del alumno que desea buscar\n");
-    scanf("%f", &dni_ingresado);
-
-    for (int i = 0; i < cant_alumnos; i++)
+    scanf("%i", &dni_ingresado);
+    bandera = 0;
+    int i = 0;
+    do
     {
         if (dni_ingresado == lista_alumnos[i].dni)
         {
             bandera = 1;
-            numero_alumno = i;
         }
-    }
+        else
+        {
+            i = i + 1;
+        }
+
+    } while (i < cant_alumnos && bandera == 0);
 
     if (bandera == 1)
     {
-        printf("DNI: %i \t nota: %i \n", lista_alumnos[numero_alumno].dni, lista_alumnos[numero_alumno].nota);
+        printf("DNI: %i \t nota: %i \n", lista_alumnos[i].dni, lista_alumnos[i].nota);
     }
     else
     {
-        printf("no se encontro un alumno con ese DNI \n");
+        printf("no se encontr%c un alumno con ese DNI \n", 162);
     }
-
-    bandera = 0;
 }
 void modificarAlumnos()
 {
     printf("ingrese DNI del alumno del cual desea modificar la nota\n");
-    scanf("%f", &dni_ingresado);
-
-    for (int i = 0; i < cant_alumnos; i++)
+    scanf("%i", &dni_ingresado);
+    int i = 0;
+    do
     {
         if (dni_ingresado == lista_alumnos[i].dni)
         {
             bandera = 1;
-            numero_alumno = i;
         }
-    }
+        else
+        {
+            i = i + 1;
+        }
+    } while (i < cant_alumnos && bandera == 0);
 
     if (bandera == 1)
     {
         printf("Ingrese la nueva nota \n");
-        scanf("%f", &nueva_nota);
-        lista_alumnos[numero_alumno].nota = nueva_nota;
+        scanf("%i", &nueva_nota);
+        lista_alumnos[i].nota = nueva_nota;
 
-        printf("DNI: %i \t nota: %i \n", lista_alumnos[numero_alumno].dni, lista_alumnos[numero_alumno].nota);
+        printf("DNI: %i \t nota: %i \n", lista_alumnos[i].dni, lista_alumnos[i].nota);
     }
     else
     {
-        printf("no se encontro un alumno con ese DNI \n");
+        printf("No se encontr%c un alumno con ese DNI \n", 162);
     }
 }
 void mostrarAlumnos()
 {
-    printf(" Lista de alumnos \n");
+    printf("Lista de alumnos \n");
     for (int i = 0; i < cant_alumnos; i++)
     {
         printf("Alumno %i \t DNI: %i \t nota: %i \n", i + 1, lista_alumnos[i].dni, lista_alumnos[i].nota);
